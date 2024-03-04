e5800b3eac64f9ce8a7e9e2ce226a1ccab6c7a64
noman5945 <abdullahnoman.5945@gmail.com>
2024-03-04 22:20:53 +0600

    Fix remain: product box resizing remainig

---
+++ src/app/pages/home/Home-Components/product-box/product-box.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-box',
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, CommonModule],
+  templateUrl: './product-box.component.html',
+  styleUrl: './product-box.component.css',
+})
+export class ProductBoxComponent {
+  @Input() fullWidthMode = false;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { ProductHeaderComponent } from './Home-Components/product-header/product-header.component';
 import { FilterComponent } from './Home-Components/filter/filter.component';
+import { ProductBoxComponent } from './Home-Components/product-box/product-box.component';
+
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 445 };
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSidenavModule, ProductHeaderComponent, FilterComponent],
+  imports: [
+    MatSidenavModule,
+    ProductHeaderComponent,
+    FilterComponent,
+    MatGridListModule,
+    ProductBoxComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   colsNum: number = 1;
+  rowHight: number = ROWS_HEIGHT[this.colsNum];
   selectedCategory: string | undefined;
   constructor() {}
 
