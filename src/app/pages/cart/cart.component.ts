a9e9404e851f84e76eea607e6d16156e309e8bcc
noman5945 <abdullahnoman.5945@gmail.com>
2024-03-06 20:34:01 +0600

    Done:Cart page design done, Ongoing:Cart page logic

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { Cart, CartItem } from '../../models/cart.model';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [RouterModule, CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
@@ -22,11 +31,33 @@ export class CartComponent implements OnInit {
         Qty: 4,
         Id: 1,
       },
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'LED',
+        price: 100,
+        Qty: 4,
+        Id: 2,
+      },
     ],
   };
   dataSource: Array<CartItem> = [];
+  //, 'Id'
+  displayColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'Qty',
+    'Total',
+    'Action',
+  ];
   constructor() {}
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.Qty)
+      .reduce((prev, current) => prev + current, 0);
   }
 }
